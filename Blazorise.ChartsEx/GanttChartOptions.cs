2a685e4549f6dabb5c5404bb2a9bf119c0b1bd5c
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-05-18 22:23:02 +0100

    - remove all options as these were copied from Bar chart

--- Blazorise.ChartsEx/GanttChartOptions.cs
+++ Blazorise.ChartsEx/GanttChartOptions.cs
@@ -1,33 +1,9 @@
 ﻿using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
 
 namespace Blazorise.ChartsEx
 {
   [DataContract]
   public class GanttChartOptions : ChartOptions
   {
-    /// <summary>
-    /// Percent (0-1) of the available width each bar should be within the category width. 1.0 will take the whole category width and put the bars right next to each other.
-    /// </summary>
-    [DataMember]
-    public float BarPercentage { get; set; } = 0.9f;
-
-    /// <summary>
-    /// Percent (0-1) of the available width each category should be within the sample width. 
-    /// </summary>
-    [DataMember]
-    public float CategoryPercentage { get; set; } = 0.8f;
-
-    /// <summary>
-    /// Manually set width of each bar in pixels. If not set, the base sample widths are calculated automatically so that they take the full available widths without overlap. Then, the bars are sized using barPercentage and categoryPercentage.
-    /// </summary>
-    [DataMember]
-    public int BarThickness { get; set; }
-
-    /// <summary>
-    /// Set this to ensure that bars are not sized thicker than this.
-    /// </summary>
-    [DataMember]
-    public int MaxBarThickness { get; set; }
   }
 }
