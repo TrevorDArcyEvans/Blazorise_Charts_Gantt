57733d76c10a4f553c72ddb9dfe595ec2ef61455
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-05-18 22:18:57 +0100

    - code cleanup

--- Blazorise.ChartsEx/Data/GanttChartDataset.cs
+++ Blazorise.ChartsEx/Data/GanttChartDataset.cs
@@ -1,51 +1,50 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
-using Blazorise.ChartsEx;
 
 namespace Blazorise.ChartsEx
 {
-    /// <remarks>
-    /// Defaults as per https://www.npmjs.com/package/chartjs-plugin-gantt
-    /// </remarks>
-    [DataContract]
-    public class GanttChartDataset<T> : ChartDataset<T>
+  /// <remarks>
+  /// Defaults as per https://www.npmjs.com/package/chartjs-plugin-gantt
+  /// </remarks>
+  [DataContract]
+  public class GanttChartDataset<T> : ChartDataset<T>
+  {
+    public GanttChartDataset() : base(
+      label: string.Empty,
+      backgroundColor: new List<string> { ChartColor.FromRgba(0, 0, 0, 0.1f) },
+      borderColor: new List<string> { ChartColor.FromRgba(0, 0, 0, 0.1f) },
+      borderWidth: 0
+    )
     {
-        public GanttChartDataset() : base(
-            label: string.Empty,
-            backgroundColor: new List<string> { ChartColor.FromRgba( 0, 0, 0, 0.1f ) },
-            borderColor: new List<string> { ChartColor.FromRgba( 0, 0, 0, 0.1f ) },
-            borderWidth: 0
-        )
-        {
-            Type = "gantt";
-        }
-
-        /// <summary>
-        /// The fill colour of the bars when hovered.
-        /// </summary>
-        /// <remarks>Default as per https://www.chartjs.org/docs/latest/configuration/elements.html#rectangle-configuration </remarks>
-        [DataMember( EmitDefaultValue = false )]
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        [JsonConverter( typeof( IndexableOptionsConverter<object> ) )]
-        public IndexableOption<object> HoverBackgroundColor { get; set; }
-
-        /// <summary>
-        /// The stroke colour of the bars when hovered.
-        /// </summary>
-        /// <remarks>Default as per https://www.chartjs.org/docs/latest/configuration/elements.html#rectangle-configuration </remarks>
-        [DataMember( EmitDefaultValue = false )]
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        [JsonConverter( typeof( IndexableOptionsConverter<object> ) )]
-        public IndexableOption<object> HoverBorderColor { get; set; }
-
-        /// <summary>
-        /// The stroke width of the bars when hovered.
-        /// </summary>
-        [DataMember]
-        public int HoverBorderWidth { get; set; } = 1;
-        
-        [DataMember]
-        public double Height { get; set; } = 0.125f;
+      Type = "gantt";
     }
+
+    /// <summary>
+    /// The fill colour of the bars when hovered.
+    /// </summary>
+    /// <remarks>Default as per https://www.chartjs.org/docs/latest/configuration/elements.html#rectangle-configuration </remarks>
+    [DataMember(EmitDefaultValue = false)]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonConverter(typeof(IndexableOptionsConverter<object>))]
+    public IndexableOption<object> HoverBackgroundColor { get; set; }
+
+    /// <summary>
+    /// The stroke colour of the bars when hovered.
+    /// </summary>
+    /// <remarks>Default as per https://www.chartjs.org/docs/latest/configuration/elements.html#rectangle-configuration </remarks>
+    [DataMember(EmitDefaultValue = false)]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonConverter(typeof(IndexableOptionsConverter<object>))]
+    public IndexableOption<object> HoverBorderColor { get; set; }
+
+    /// <summary>
+    /// The stroke width of the bars when hovered.
+    /// </summary>
+    [DataMember]
+    public int HoverBorderWidth { get; set; } = 1;
+
+    [DataMember]
+    public double Height { get; set; } = 0.125f;
+  }
 }
